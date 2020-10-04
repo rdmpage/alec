0053d43917ec8fb995b35e6a5845a81b349d9cb1
Roderic Page <rdmpage@gmail.com>
2020-10-04 10:43:23 +0100

    Add errata

---
+++ api_work_errata.php
@@ -0,0 +1,92 @@
+<?php
+
+// Does work have errata / is this work an erratum?
+
+error_reporting(E_ALL);
+
+require_once(dirname(__FILE__) . '/config.inc.php');
+require_once(dirname(__FILE__) . '/wikidata_api.php');
+
+
+$id = 'Q99931585';
+
+if (isset($_REQUEST['id']))
+{
+	$id = $_REQUEST['id'];
+}
+
+
+
+$query = 'PREFIX schema: <http://schema.org/>
+	PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+	CONSTRUCT 
+	{
+		<http://example.rss>
+		rdf:type schema:DataFeed;
+		schema:name "Corrects / corrected by";
+		schema:dataFeedElement ?item .
+
+		?item 
+			rdf:type schema:DataFeedItem;
+			rdf:type ?item_type;
+			schema:name ?title;
+			schema:datePublished ?datePublished;
+			schema:description ?description;	
+			
+# doi
+ schema:identifier ?doi_identifier .
+					 ?doi_identifier rdf:type schema:PropertyValue .
+					 ?doi_identifier schema:propertyID "doi" .
+					 ?doi_identifier schema:value ?doi .
+ 							
+	}
+	WHERE
+{
+  # person
+  VALUES ?work { wd:' . $id . ' }
+    
+    {
+    	?item wdt:P2507 ?work .
+    }
+    UNION
+    {
+    	?work wdt:P2507 ?item .
+    }
+	
+	?item wdt:P31 ?type .
+
+	?item wdt:P1476 ?title .
+
+	OPTIONAL {
+	?item wdt:P577 ?date .
+	BIND(STR(?date) as ?datePublished) 
+	}  		
+	
+  # Make DOI lowercase
+ OPTIONAL {
+   ?item wdt:P356 ?doi_string .   
+   BIND( IRI (CONCAT (STR(?item), "#doi")) as ?doi_identifier)
+   BIND( LCASE(?doi_string) as ?doi)
+  }
+ }';
+
+
+
+$callback = '';
+if (isset($_GET['callback']))
+{
+	$callback = $_GET['callback'];
+}
+
+if ($callback != '')
+{
+	echo $callback . '(';
+}
+echo sparql_construct_stream($config['sparql_endpoint'], $query);
+if ($callback != '')
+{
+	echo ')';
+}
+
+
+?>
